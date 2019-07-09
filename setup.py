76ff24f21fbace5a0dedadf197852e2575e346c9
Lin8x <40849996+Lin8x@users.noreply.github.com>
2019-07-08 21:03:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ p = platform.system()
 if "Linux" == p:
    os.system("sudo apt-get install googler")
    os.system("sudo mv googleot-file /usr/share")
-   print("Moved googleot-file (the python programs) to '/usr/share'!")
+   print("[+] Moved googleot-file (the python programs) to '/usr/share'!")
    os.system("sudo mv googleot /usr/bin")
-   print("Moved googleot (the bash program) to '/usr/bin'!")
+   print("[+] Moved googleot (the bash program) to '/usr/bin'!")
    os.system("sudo mv googleot.desktop /usr/share/applications")
-   print("Moved googleot.desktop (the application program) to '/usr/share/applications'!")
-   print("Please type 'googleot' in your terminal to run the program or open from the application!")
+   print("[+] Moved googleot.desktop (the application program) to '/usr/share/applications'!")
+   print("[-] Please type 'googleot' in your terminal to run the program or open from the application!")
 else:
    os.system("brew install googler")
-   print("Please do 'sudo python3 googleot-files/runme.py' to run the program!")
+   print("[-] Please do 'sudo python3 googleot-files/runme.py' to run the program!")
 
    
