5e96249e8af9f9d385e606f19028ccf5e618e0a3
Lin8x <40849996+Lin8x@users.noreply.github.com>
2019-07-08 20:59:51 -0400

    Update uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -6,8 +6,8 @@ import platform
 p = platform.system()
 if "Linux" == p:
    os.system("sudo rm -rf /usr/share/googleot-file")
-   os.system("sudo rm -rf /usr/bin/googleot")
-   os.system("sudo rm -rf /usr/share/applications/googleot.desktop")
+   os.system("sudo rm /usr/bin/googleot")
+   os.system("sudo rm /usr/share/applications/googleot.desktop")
    print("Uninstalled Googleot. Come back again! :)")
 else:
    print("Just delete the file. :/")
