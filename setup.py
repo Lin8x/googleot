e99f95020a6aca895b17912923d898279141754a
Lin8x <40849996+Lin8x@users.noreply.github.com>
2019-07-08 20:32:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 
 import os
+import platform
 
-os.system("sudo mv googleot-file /usr/share")
-print("Moved googleot-file (the python programs) to '/usr/share'!")
-os.system("sudo mv googleot /usr/bin")
-print("Moved googleot (the bash program) to '/usr/bin'!")
-os.system("sudo mv googleot.desktop /usr/share/applications")
-print("Moved googleot.desktop (the application program) to '/usr/share/applications'!")
-print("Please type 'googleot' in your terminal to run the program or open from the application!")
+os.system("pip3 install smtplib")
+os.system("pip3 install stdiomask")
+os.system("pip3 install imaplib")
+os.system("pip3 install getpass")
+
+p = platform.system()
+if "Linux" == p:
+   os.system("sudo apt-get install googler")
+   os.system("sudo mv googleot-file /usr/share")
+   print("Moved googleot-file (the python programs) to '/usr/share'!")
+   os.system("sudo mv googleot /usr/bin")
+   print("Moved googleot (the bash program) to '/usr/bin'!")
+   os.system("sudo mv googleot.desktop /usr/share/applications")
+   print("Moved googleot.desktop (the application program) to '/usr/share/applications'!")
+   print("Please type 'googleot' in your terminal to run the program or open from the application!")
+else:
+   os.system("brew install googler")
+   print("Please do 'sudo python3 googleot-files/runme.py' to run the program!")
